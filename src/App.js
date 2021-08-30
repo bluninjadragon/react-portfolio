e7311bb1513d0fe34e1a3caf4ab856737573423e
bluninjadragon <bluninjadragon@gmail.com>
2021-08-30 17:45:26 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MyNavbar from "./components/MyNavbar/MyNavbar.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+    <MyNavbar />
+  </div>;
 }
 
 export default App;
