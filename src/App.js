d6481094828d0ee0771e3d0304a1646e6ad6c58c
bluninjadragon <jwhbootcamp@gmail.com>
2021-08-30 22:29:53 -0500

    updated app.js to import components and react-bootstrap effects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
+import Container from "react-bootstrap/Container";
+import Image from "./assets/images/parallax/25101.webp";
+import Slide from "react-reveal/Slide";
+import Fade from "react-reveal/Fade";
+
 import IntroBanner from "./components/IntroBanner/IntroBanner";
 import MyNavbar from "./components/MyNavbar/MyNavbar";
 
 function App() {
-  return <div className="App">
+  return (
+  <div className="App">
     <MyNavbar fixed="top" />
-    <IntroBanner />
-  </div>;
-}
+
+  </div>);
+};
 
 export default App;
